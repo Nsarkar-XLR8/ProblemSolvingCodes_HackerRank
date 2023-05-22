3b8e9465870c58d3233e3cf0991019d6144dc522
Nsarkar-XLR8 <76911177+Nsarkar-XLR8@users.noreply.github.com>
2023-05-22 20:59:02 +0600

    Update Java_Abstract_Class.java

--- src/Java_Abstract_Class.java
+++ src/Java_Abstract_Class.java
@@ -1,3 +1,8 @@
+/* The abstract keyword is a non-access modifier, used for classes and methods:
+Abstract class: is a restricted class that cannot be used to create objects (to access it, it must be inherited from another class). 
+Abstract method: can only be used in an abstract class, and it does not have a body. */
+
+
 import java.util.*;
 abstract class Book{
     String title;
@@ -29,3 +34,4 @@ public class Java_Abstract_Class {
 
     }
 }
+
