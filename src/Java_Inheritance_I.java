395133512cea0a9304eb6ea65ed7da337fafc1cb
Nsarkar-XLR8 <76911177+Nsarkar-XLR8@users.noreply.github.com>
2023-05-22 21:36:43 +0600

    Update Java_Inheritance_I.java

--- src/Java_Inheritance_I.java
+++ src/Java_Inheritance_I.java
@@ -1,3 +1,6 @@
+/* Inheritance in Java is a concept that acquires the properties from one class to other classes; for example, the relationship between father and son.
+Inheritance in Java is a process of acquiring all the behaviours of a parent object. */
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
